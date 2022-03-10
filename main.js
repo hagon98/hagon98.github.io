e6b37c71131af7e255006583e0f319f5e07aa38c
hagon98 <79465063+hagon98@users.noreply.github.com>
2022-03-10 12:55:24 +0100

    test front deploiement

---
+++ main.js
@@ -0,0 +1,63 @@
+const contenu = document.querySelector(".contenu");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const btnConnexion = document.querySelector("#seConnecter");
+btnConnexion.addEventListener("click", () => {
+  envoiUser(username.value, password.value);
+});
+
+function envoiUser(username, password) {
+  let urlChecking = "https://quiet-dusk-76059.herokuapp.com/api/login_check";
+
+  let bodyRequete = {
+    username: username,
+    password: password,
+  };
+  let requete = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(bodyRequete),
+  };
+  fetch(urlChecking, requete)
+    .then((reponse) => reponse.json())
+    .then((reponseDeserialisee) => {
+      console.log(reponseDeserialisee);
+
+      afficherMessage(reponseDeserialisee.token);
+      document.querySelector("#username").value = "";
+      document.querySelector("#password").value = "";
+    });
+}
+
+function afficherMessage(token) {
+  contenu.innerHTML = "";
+  let urlGood = "https://quiet-dusk-76059.herokuapp.com/api/";
+
+  let requete = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  fetch(urlGood, requete)
+    .then((reponse) => reponse.json())
+    .then((form) => {
+      console.log(form);
+      form.forEach((message) => {
+        testUn = `<div>
+            <hr>
+                <h2> Message : ${message.test}</h2>
+    </div>
+`;
+        contenu.innerHTML += testUn;
+      });
+    });
+  //   if (username && password) {
+  //     return urlGood;
+  //   } else {
+  //     echo("mauvaise infos");
+  //   }
+}
